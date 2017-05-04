d75870f3f8264a906706da09e9ea5a71e200946a
ekorab <ekorab@o2.pl>
2017-05-04 14:45:02 +0200

    gmail automation

--- tests/features/step_definitions/custom_steps.rb
+++ tests/features/step_definitions/custom_steps.rb
@@ -4,45 +4,20 @@ require 'appium_lib'
 # Add your custom steps here
 # $driver is instance of webdriver use this instance to write your custom code
 
-
-
 Given(/^I open test page$/) do
 	link = 'file:///'
   	link+= File.absolute_path("test_page.html")
   	navigate_to(link) 
 end
 
-# Then(/^I press "(.*?)" key from keyboard$/) do |key|
-# 	$driver.action.send_keys(:"#{key}").perform
-# end
-
-# Then(/^Then I press  combination of "(.*?)" and "(.*?)" keys from keyboard$/) do |key1, key2|
-# 	$driver.action.key_down(:"#{get_key}").send_keys(:"#{in_out}").key_up(:"#{get_key}").perform
-# end
-
-# Then(/^I long press on element having (.+) "(.*?)"$/) do |type, access_name|
-#   validate_locator type
-#   long_press_1(type, access_name, 5)
-# end
-
-# def long_press_1(type, access_name, duration)
-#   element = WAIT.until { $driver.find_element(:"#{type}" => "#{access_name}") }
-#   #parameters = { "element" => "#{element}", "duration" => "#{duration}" }
-#   #args = parameters.select { |k, v| [:element, :duration].include? k }
-#   #args = args_with_ele_ref(args)
-#   #chain_method(:longPress, args)
-#   #Appium::TouchAction.new.tap(element: "#{element}", count: "#{duration}").perform
-#   Appium::TouchAction.new.long_press(element:element, x:10, y:10, duration:duration).perform()
-# end
-
 Then(/^I should get logged in$/) do
   verify_username
 end
 
 
-def verify_username
+def verify_username(username)
   ele_user_name = $driver.find_element(:xpath, "/html/body/div[7]/div[3]/div/div[1]/div[4]/div[1]/div[1]/div[1]/div/div[4]/div[1]/a")
-  if ele_user_name.text !="qasage.test@gmail.com"
+  if ele_user_name.text !=@username
     raise "Invalid user name"
   else
     puts "User gets logged in successfully"
